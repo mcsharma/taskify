8a22e3763a3178667aa1b1de1b47a0046cbab62f
mahesh <mahesh.sharma@thoughtspot.com>
2016-07-21 21:00:08 -0700

    First commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?hh
+echo '
+<html>
+<head>
+  <script type="text/javascript" src="public/bundle.js"></script>
+</head>
+<body>
+  <div id="root"></div>
+</body>
+</html>';
---
+++ public/main.js
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+window.onload = function () {
+    ReactDOM.render(
+        <span>Hello reiact3</span>,
+        document.getElementById("root")
+    );
+};
+
+
