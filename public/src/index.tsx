b9c3c31e0deb7a256536cf11017c9d2ce16369ef
Mahesh <mahesh.sharma@thoughtspot.com>
2016-09-07 02:35:05 -0700

    Added less-loaded in webpack to compile less into css

--- public/src/index.tsx
+++ public/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Root } from "./components/Root";
 
-require('./css/index.css');
+require('./css/index.less');
 
 ReactDOM.render(
     <Root />,
--- public/webpack.config.js
+++ public/webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
             { 
                 test: /\.css$/, 
                 loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
             }
         ],
 
