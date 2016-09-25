228aa2d1ee661589c3db7d95e0836fbe21bbdf54
Mahesh <mcsharma1990@gmail.com>
2016-09-25 19:54:29 +0000

    Added hhvm fatal error handler to print errors on screen

--- server/index.php
+++ server/index.php
@@ -1,4 +1,6 @@
 <?hh
+// Nothing must be placed before this call.
+set_error_handler(get_error_handler());
 
 require_once('api/ApiServer.php');
 
@@ -31,4 +33,25 @@ if (substr($path, 0, 4) === 'api/') {
   }
 } else {
   echo 'Taskify API Server';
+}
+
+
+function get_error_handler() {
+  return function ($errorNumber, $message, $errfile, $errline) {
+    switch ($errorNumber) {
+        case E_ERROR:
+            $errorLevel = 'Error';
+            break;
+        case E_WARNING:
+            $errorLevel = 'Warning';
+            break;
+        case E_NOTICE:
+            $errorLevel = 'Notice';
+            break;
+        default:
+            $errorLevel = 'Undefined';
+    }
+    echo '<br/><b>'.$errorLevel.'</b>: '.$message.' in <b>'.
+      $errfile.'</b> on line <b>'.$errline.'</b><br/>';
+  };
 }
